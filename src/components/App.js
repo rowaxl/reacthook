0f8a701dc4d99d624b9f5c30199d035f7cd5c5c7
rowaxl <rowaxl0@gmail.com>
2019-03-14 10:53:25 +0900

    update app overview

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    state = { resources: 'posts' };
+
     render() {
         return (
             <div className="ui container">
-                app
+                <div>
+                    <button
+                        onClick={() => this.setState({ resources: 'posts' })}
+                    >
+                        Posts
+                    </button>
+                    <button
+                        onClick={() => this.setState({ resources: 'todos' })}
+                    >
+                        Todos
+                    </button>
+                </div>
+                <div>
+                    { this.state.resources }
+                </div>
             </div>
         )
     }
