1db15224a14f301fdedc9c4d02333df4fc6a6c39
rowaxl <rowaxl0@gmail.com>
2019-03-15 23:12:57 +0900

    add component did update

--- src/components/ResourceList.js
+++ src/components/ResourceList.js
@@ -9,6 +9,13 @@ class ResourceList extends React.Component {
         this.setState({ resources: response.data });
     }
 
+    componentDidUpdate = async prevProps => {
+        if (prevProps.resource !== this.props.resource) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+            this.setState({ resources: response.data });
+        }
+    }
+
     render() {
         return (
             <div>
